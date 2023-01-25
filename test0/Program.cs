e6edb608a1c19b677293e5780d0d4aafe2d2219a
killerror <rmurzaliev@yandex.ru>
2023-01-26 00:06:42 +0400

    add arrays printing

--- test0/Program.cs
+++ test0/Program.cs
@@ -21,3 +21,12 @@ string[] finalArray = new string[finalArraySize];
 for (int i = 0; i < originArray.Length; i++)
     if (originArray[i].Length <= 3)
         finalArray[finalArrayIndx++] = originArray[i];
+
+Console.Write("Исходный массив: ");
+for (int i = 0; i < originArray.Length; i++)
+    Console.Write(originArray[i] + ((i != originArray.Length-1) ? ", " : ""));
+Console.WriteLine();
+
+Console.Write("Результирующий массив: ");
+for (int i = 0; i < finalArray.Length; i++)
+    Console.Write(finalArray[i] + ((i != finalArray.Length-1) ? ", " : ""));
