260c8d7f30ce6237ad0e44011a3b04d3f8687005
killerror <rmurzaliev@yandex.ru>
2023-01-25 23:41:57 +0400

    add main code

--- test0/Program.cs
+++ test0/Program.cs
@@ -4,7 +4,20 @@
     "Liam",
     "Mia",
     "Henry",
+    "Yu",
+    "Leo",
     "David",
-    "Leo"
+    "Ed"
 };
 
+int finalArraySize = 0;
+int finalArrayIndx = 0;
+
+for (int i = 0; i < originArray.Length; i++)
+    if (originArray[i].Length <= 3) finalArraySize++;
+
+string[] finalArray = new string[finalArraySize];
+
+for (int i = 0; i < originArray.Length; i++)
+    if (originArray[i].Length <= 3)
+        finalArray[finalArrayIndx++] = originArray[i];
