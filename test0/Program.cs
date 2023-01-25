1b02c4ae2ffb0dca2c13eee291292869c3bbbf71
killerror <rmurzaliev@yandex.ru>
2023-01-25 23:29:29 +0400

    add originArray

--- test0/Program.cs
+++ test0/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] originArray = {
+    "Mary",
+    "Joe",
+    "Liam",
+    "Mia",
+    "Henry",
+    "David",
+    "Leo"
+};
+
